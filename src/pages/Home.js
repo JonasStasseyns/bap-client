9814db5c1f4ec2f8f759bd13e6b1fba82274a61b
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2021-04-02 16:23:04 +0200

    glitchio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Home = () => {
+
+    useEffect(() => axios.get("https://bachelorproef.stasseynsjonas.be/api/v1/").then(res => console.log(res)), [])
+
     return (
         <div className="home-wrapper">
             <h1>HomePage</h1>
